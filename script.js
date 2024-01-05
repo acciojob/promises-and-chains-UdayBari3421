2b92ff91b707e676905f31e941238903e6582a5e
Uday Bari <115573575+UdayBari3421@users.noreply.github.com>
2024-01-05 19:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    
+    const nameInput = document.getElementById("name");
+    const ageInput = document.getElementById("age");
+
+    if (nameInput.value.trim() === "" || ageInput.value.trim() === "") {
+        console.log("Please fill out all fields");
+        return;
+    }
+
+    const formPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const age = parseInt(ageInput.value);
+
+            if (age >= 18) {
+                resolve(`Welcome, ${nameInput.value}. You can vote.`);
+            } else {
+                reject(`Oh sorry, ${nameInput.value}. You aren't old enough.`);
+            }
+        }, 4000); 
+    });
+
+    formPromise.then((message) => {
+        console.log(message);
+    }).catch((message) => {
+		console.log(message);
+	});
+});
